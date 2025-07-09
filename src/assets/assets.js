455366a4c63d2f81db3d928707164e301522e051
= <108467703+Hamdullah-Andar@users.noreply.github.com>
2025-07-09 14:49:04 +0430

    Add company logos and display them on the homepage
    - Added SVG files for Adobe, Microsoft, Walmart, Accenture, and PayPal logos.
    - Updated assets.js to include new logo imports.
    - Created a Companies component to display the logos.
    - Integrated the Companies component into the Home page layout.

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -3,11 +3,21 @@ import user_icon from './user.svg'
 import hero_sketch from './sketch.svg'
 import hero_sketch2 from './sketch2.svg'
 import search_icon from './search.svg'
+import microsoft_logo from './microsoft.svg'
+import walmart_logo from './walmart.svg'
+import accenture_logo from './accenture.svg'
+import adobe_logo from './adobe.svg'
+import paypal_logo from './paypal.svg'
 
 export const assets = {
     web_Logo,
     user_icon,
     hero_sketch,
     hero_sketch2,
-    search_icon
+    search_icon,
+    microsoft_logo,
+    walmart_logo,
+    accenture_logo,
+    adobe_logo,
+    paypal_logo
 }
