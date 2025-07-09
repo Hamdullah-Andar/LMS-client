b8580ebd85cb75957b6567d3643affdaa8f0cea7
= <108467703+Hamdullah-Andar@users.noreply.github.com>
2025-07-09 13:38:04 +0430

    feat: add SearchBar component with search functionality and integrate search icon

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -2,10 +2,12 @@ import web_Logo from './logo.svg'
 import user_icon from './user.svg'
 import hero_sketch from './sketch.svg'
 import hero_sketch2 from './sketch2.svg'
+import search_icon from './search.svg'
 
 export const assets = {
     web_Logo,
     user_icon,
     hero_sketch,
-    hero_sketch2
+    hero_sketch2,
+    search_icon
 }
