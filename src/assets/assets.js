0038608473a1233a782fe1cd8a717158ab92bba9
= <108467703+Hamdullah-Andar@users.noreply.github.com>
2025-07-09 11:38:20 +0430

    feat: add new hero component with responsive design and SVG asset
    - Introduced a new SVG asset (sketch2.svg) for visual enhancement.
    - Updated Hero component to include the new SVG and improved text content.
    - Added responsive text sizes using CSS design tokens for better readability.
    - Integrated Hero component into the Home page for a cohesive layout.

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,7 +1,11 @@
 import web_Logo from './logo.svg'
 import user_icon from './user.svg'
+import hero_sketch from './sketch.svg'
+import hero_sketch2 from './sketch2.svg'
 
 export const assets = {
     web_Logo,
-    user_icon
+    user_icon,
+    hero_sketch,
+    hero_sketch2
 }
